ba1d844b406f5dccf69027a6558b1f4f04b060a8
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-10 00:45:53 +0900

    準備

--- test_cython/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-from numpy import get_include # cimport numpy を使うため
-
-ext = Extension("test_1", sources=["test_1.pyx"], include_dirs=['.', get_include()])
-setup(name="test_1", ext_modules=cythonize([ext]))
